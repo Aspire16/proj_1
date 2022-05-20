bd4f588c34894d6f45c0258a9e09cf9d4b53e417
Aspire16 <sypar@yandex.ru>
2022-05-20 13:16:26 +0700

    main

---
+++ first.py
@@ -0,0 +1,13 @@
+a = 11
+b = 6
+""" print (a % b)
+print ((23 + 8) % 24)
+user_name = input('input hire ')
+print ('Hi', user_name)
+pre_str = 'тест'
+test_str = pre_str + ' ' + user_name
+print (test_str)
+print (f'{a} {b}  {user_name}') """
+lis_ras = ["lis 1", "lis 2", "lis 3", "lis 4", "lis 5"]
+print (type(lis_ras))
+print (lis_ras)
