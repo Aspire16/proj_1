d0e73fe12a236c99d450861ce5901e2fa0d2993c
Aspire16 <sypar@yandex.ru>
2022-05-23 14:44:11 +0700

    Обновил проверки в функции

--- 2_srt.py
+++ 2_srt.py
@@ -10,57 +10,26 @@
 * Вызвать функцию несколько раз, передавая ей разные праметры 
   и выводя на экран результаты
 """
-import re
 
-
-def MyF_chck_input(itype, l_message):  # проверка ввода на (Хочу универсальный)
-    while True:                                         # запускаю цикл опроса ввода до получения требуемого значения
-        getVal = input(l_message).strip()               # Ввод переменной с удаление пробелов
-        try:                                            # Проверка что  преобразуется в число без ошибки
-            getTempVal = int(getVal)
-        except ValueError:                              # Проверка на ошибку неверного формата (введены буквы)
-            if itype == "t":                            # Если требуется текстовая переменная
-                return getVal
-        else:                                           # Если преобразован в число без ошибки
-            return getTempVal
-        try:
-            getTempVal = float(getVal)
-        except ValueError:
-            if itype == "t":                            # Если требуется текстовая переменная
-                return getVal
-        else:                                           # Если преобразован в число без ошибки
-            return getTempVal
-
-def MyF_chck_sing_str (str):
-    try:
-        test_str = int(str)
-        return False
-    except ValueError:
-        return True
-    try:
-        test_str = float(str)
-        return False
-    except ValueError:
-        return True
 
 
 
 def MyF_chck_2_srt (str1, str2):
-    if MyF_chck_sing_str (str1) == MyF_chck_sing_str (str2):
+    if str1.isdigit() or str2.isdigit():
+        return 0
+    else:
         if str1 == str2:
             return 1
+        else:
             if len(str1) > len(str2):
                 return 2
-            if str1 == "learn":
-                return 2            
-    else:
-        return "Не равны"  
-    
+            if str2 == "learn":
+                return 3
 
 
 def MyF_quwest():
-    first_str = MyF_chck_input("t", "первая строка для сравнения: ")        #первую строку спрашиваем
-    second_str = MyF_chck_input("t", "вторая строка для сравнения: ")       #вторую спрашиваем
+    first_str = input("первая строка для сравнения: ")        #первую строку спрашиваем
+    second_str = input("вторая строка для сравнения: ")       #вторую спрашиваем
     print (MyF_chck_2_srt(first_str, second_str))
 
    
